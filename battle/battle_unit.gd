48d258dd3481aa14b44a2eafc5ae8071039f693d
Ryan V <47340962+denvermullets@users.noreply.github.com>
2023-04-18 08:32:32 -0400

    feat(battle): add turnStack, turnManager, base of turn structure (#1)

--- battle/battle_unit.gd
+++ battle/battle_unit.gd
@@ -1,5 +1,16 @@
 extends Node2D
 class_name BattleUnit
 
-func melee_attack() -> void:
+var turnStack: TurnStack = ReferenceStash.turnStack
+
+func melee_attack(target: BattleUnit) -> void:
+  turnStack.add_node(self)
   print("Attack")
+  target.take_hit(self)
+  turnStack.remove_node(self)
+
+func take_hit(attacker: BattleUnit) -> void:
+  # if we had a knockback animation it would trigger here, possibly
+  turnStack.add_node(self)
+  print("Taking hit")
+  turnStack.remove_node(self)
